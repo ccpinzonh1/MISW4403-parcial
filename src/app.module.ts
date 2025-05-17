3566a4dc10f05ed47e0dbccb5d42fe50f0a3ae05
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2025-05-16 21:19:46 -0500

    feat: add PostgreSQL support with TypeORM configuration and pg dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BibliotecaModule } from './biblioteca/biblioteca.module';
 import { LibroModule } from './libro/libro.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BibliotecaEntity } from './biblioteca/biblioteca.entity';
+import { LibroEntity } from './libro/libro.entity/libro.entity';
 
 @Module({
-  imports: [BibliotecaModule, LibroModule],
+  imports: [
+    BibliotecaModule,
+    LibroModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'biblioteca-db',
+      entities: [LibroEntity, BibliotecaEntity],
+      dropSchema: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
