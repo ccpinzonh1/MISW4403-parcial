853ae5149b2d69a7af112355ac4bf3249b8eb804
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2025-05-16 22:00:46 -0500

    Defina la lógica de Libro, esta debe incluir los métodos findAll, findOne, create, update y delete. En los métodos create y update, valide que la fecha de publicación sea anterior o igual a la fecha actual.

--- src/libro/libro.module.ts
+++ src/libro/libro.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { LibroService } from './libro.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LibroEntity } from './libro.entity/libro.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([LibroEntity])],
+  providers: [LibroService],
+})
 export class LibroModule {}
---
+++ src/libro/libro.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { LibroEntity } from './libro.entity/libro.entity';
+import {
+  BusinessLogicException,
+  BusinessError,
+} from '../shared/errors/business-errors';
+
+@Injectable()
+export class LibroService {
+  constructor(
+    @InjectRepository(LibroEntity)
+    private readonly libroRepository: Repository<LibroEntity>,
+  ) {}
+
+  async findAll(): Promise<LibroEntity[]> {
+    return this.libroRepository.find({ relations: ['bibliotecas'] });
+  }
+
+  async findOne(id: string): Promise<LibroEntity> {
+    const libro = await this.libroRepository.findOne({
+      where: { id },
+      relations: ['bibliotecas'],
+    });
+    if (!libro) {
+      throw new BusinessLogicException(
+        `Libro with ID ${id} not found`,
+        BusinessError.NOT_FOUND,
+      );
+    }
+    return libro;
+  }
+
+  async create(libro: LibroEntity): Promise<LibroEntity> {
+    if (libro.fechaPublicacion > new Date()) {
+      throw new BusinessLogicException(
+        'La fecha de publicación debe ser anterior o igual a la fecha actual.',
+        BusinessError.PRECONDITION_FAILED,
+      );
+    }
+    return this.libroRepository.save(libro);
+  }
+
+  async update(id: string, libro: LibroEntity): Promise<LibroEntity> {
+    const existingLibro = await this.findOne(id);
+    if (libro.fechaPublicacion > new Date()) {
+      throw new BusinessLogicException(
+        'La fecha de publicación debe ser anterior o igual a la fecha actual.',
+        BusinessError.PRECONDITION_FAILED,
+      );
+    }
+    const updatedLibro = { ...existingLibro, ...libro };
+    return this.libroRepository.save(updatedLibro);
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.libroRepository.delete(id);
+  }
+}
